363b7205d034869a424a24f3378f00e259ebf8f1
Filipe <fedzn11@gmail.com>
2021-12-09 15:22:10 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+// Não podemos criar variáveis com palavras reservadas
+// Variáveis precisam ter nomes significativos
+// Não pode começar o nome de uma variável com um número
+// Não podem contar espaços ou traços
+// Utilizamos camelCase
+// Case-sensitive
+// Não podemos redeclarar variáreis com let
+// NÃO UTILIZE VAR, UTILIZE LET.
+let nomeCliente = 'Filipe';
+nomeCliente = 'Neves';
+
+console.log(nomeCliente)
